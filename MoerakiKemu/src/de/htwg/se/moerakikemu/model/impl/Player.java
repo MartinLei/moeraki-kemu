083dae2962541a17c6e9e118a6856971ece0f3d3
MartinLei <malei@htwg-konstanz.de>
2017-03-25 19:39:27 +0100

    fix sonar

--- MoerakiKemu/src/de/htwg/se/moerakikemu/model/impl/Player.java
+++ MoerakiKemu/src/de/htwg/se/moerakikemu/model/impl/Player.java
@@ -2,38 +2,48 @@ package de.htwg.se.moerakikemu.model.impl;
 
 import de.htwg.se.moerakikemu.model.IPlayer;
 
+/**
+ * Implementation of the player
+ */
 public class Player implements IPlayer {
 
-	int points;
-	
+	private int points;
 	private String name;
-	
+
+	/**
+	 * Initial a new Player with no points and non name
+	 */
 	public Player() {
 		name = "";
 		points = 0;
 	}
-	
-	public int getPoints(){
+
+	@Override
+	public int getPoints() {
 		return points;
 	}
-	
-	public String getName(){
+
+	@Override
+	public String getName() {
 		return name;
 	}
-	
-	 public void addPoints(int amount){
-		 if (amount >= 0) {
-			 points += amount;
-		 }
-	 }
-	 
-	 public void setName (String name){
-		 if (name != null) {
-			 this.name = name;
-		 }
-	 }
-	 
-	 public void refreshPoints(){
-		 points = 0;
-	 }
+
+	@Override
+	public void addPoints(int amount) {
+		if (amount >= 0) {
+			points += amount;
+		}
+	}
+
+	@Override
+	public void setName(String name) {
+		if (name != null) {
+			this.name = name;
+		}
+	}
+
+	@Override
+	public void refreshPoints() {
+		points = 0;
+	}
 }
