9a7beacade16e106637a61ac105418cdad6f66f3
MartinLei <malei@htwg-konstanz.de>
2017-03-25 11:56:20 +0100

    add override anotation

--- MoerakiKemu/src/de/htwg/se/moerakikemu/controller/impl/ControllerPlayer.java
+++ MoerakiKemu/src/de/htwg/se/moerakikemu/controller/impl/ControllerPlayer.java
@@ -11,69 +11,80 @@ public class ControllerPlayer implements IControllerPlayer {
 	private IPlayer player1;
 	private IPlayer player2;
 	private IPlayer startDot;
-	
+
 	private IPlayer currentPlayer;
-	
-	public ControllerPlayer(){
+
+	public ControllerPlayer() {
 		player1 = new Player();
 		player2 = new Player();
 		startDot = new Player();
 		currentPlayer = startDot;
 		startDot.setName("StartDot");
 	}
-	
-	public void newGame(){
+
+	@Override
+	public void newGame() {
 		player1.refreshPoints();
 		player2.refreshPoints();
 		currentPlayer = startDot;
 	}
-	
+
+	@Override
 	public void setName(String player1name, String player2name) {
 		player1.setName(player1name);
 		player2.setName(player2name);
 	}
-	
+
+	@Override
 	public String getPlayer1Name() {
 		return player1.getName();
 	}
 
+	@Override
 	public String getPlayer2Name() {
 		return player2.getName();
 	}
 
+	@Override
 	public int getPlayer1Points() {
 		return player1.getPoints();
 	}
 
+	@Override
 	public int getPlayer2Points() {
 		return player2.getPoints();
 	}
-	
-	public void addAPointPlayer1(){
+
+	@Override
+	public void addAPointPlayer1() {
 		player1.addPoints(1);
 	}
 
+	@Override
 	public void addAPointPlayer2() {
 		player2.addPoints(1);
 	}
-	
+
+	@Override
 	public void selectNextPlayer() {
-		if(currentPlayer == player1) {
+		if (currentPlayer == player1) {
 			currentPlayer = player2;
-		} else if (currentPlayer == player2){
+		} else if (currentPlayer == player2) {
 			currentPlayer = player1;
 		} else {
 			currentPlayer = player1;
 		}
 	}
-	
-	public boolean startDotSet(){
-		if("StartDot".equals(currentPlayer.getName())){
+
+	@Override
+	public boolean startDotSet() {
+		if ("StartDot".equals(currentPlayer.getName())) {
 			return false;
 		}
 		return true;
 	}
 
+	@Override
 	public String getCurrentPlayerName() {
 		return currentPlayer.getName();
 	}
@@ -87,6 +98,5 @@ public class ControllerPlayer implements IControllerPlayer {
 	public void setPlayer2Name(String name) {
 		player2.setName(name);
 	}
-
 
 }
